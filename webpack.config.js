bc5e6c3d5417f99181a9183795d0e1eebf4cfaea
Jason Yang <syan19@live.cn>
2020-01-07 20:29:40 +1100

    second save

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const postCssPlugins = [
+    require('postcss-import'),
+    require('postcss-simple-vars'),
+    require('postcss-nested')
+]
+
+module.exports = {
+    entry: './app/assets/scripts/index.js',
+    output: {
+        path: path.resolve(__dirname, 'app'),
+        filename: 'bundled.js'
+    },
+    mode: 'development',
+    watch: true,
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [
+                    'style-loader', 'css-loader', {loader: 'postcss-loader', options: {plugins: postCssPlugins}}
+                ]
+            }
+        ]
+    }
+}
