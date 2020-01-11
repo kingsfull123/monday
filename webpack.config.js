5ecb1b15de53528620fcd03dfcc6378418f1f9ac
Jason Yang <syan19@live.cn>
2020-01-11 21:49:13 +1100

    complete three sections

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const postCssPlugins = [
     require('postcss-import'),
+    require('postcss-mixins'),
     require('postcss-simple-vars'),
     require('postcss-nested')
 ]
@@ -11,8 +12,16 @@ module.exports = {
         path: path.resolve(__dirname, 'app'),
         filename: 'bundled.js'
     },
+    devServer: {
+        before: function(app, server) {
+            server._watch('./app/**/*.html')
+        },
+        contentBase: path.join(__dirname, 'app'),
+        hot: true,
+        port: 3000,
+        host: '0.0.0.0'
+    },
     mode: 'development',
-    watch: true,
     module: {
         rules: [
             {
